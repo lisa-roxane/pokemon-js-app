e3d379b989576f90a136ecb2452a6c5cbcb41ea4
Lisa <lisa.roxane@gmail.com>
2022-01-18 11:35:19 -0330

    Update scripts.js
    Create array of objects with keys and values

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert('Hello world');
-let favoriteFood="Pineapple";
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur', 
+        height: 0.7, 
+        types: ['Grass', 'Poison']
+    }, 
+    {
+        name: 'Charizard', 
+        height: 1.7, 
+        types: ['Fire', 'Flying']
+    }, 
+    {
+        name: 'Butterfree', 
+        height: 1.1, 
+        types: ['Bug', 'Flying']
+    }, 
+    {
+        name: 'Nidoqueen', 
+        height: 1.3, 
+        types: ['Ground', 'Poison']
+    }
+];
