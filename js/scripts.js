9cdc287f3c57ebedf691408e242c32ddcf5d39f2
Lisa <lisa.roxane@gmail.com>
2022-01-25 15:34:07 -0330

    Update scripts.js
    Add IIFE, updated forEach() loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,55 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur', 
-        height: 0.7, 
-        types: ['Grass', 'Poison']
-    }, 
-    {
-        name: 'Charizard', 
-        height: 1.7, 
-        types: ['Fire', 'Flying']
-    }, 
-    {
-        name: 'Butterfree', 
-        height: 1.1, 
-        types: ['Bug', 'Flying']
-    }, 
-    {
-        name: 'Nidoqueen', 
-        height: 1.3, 
-        types: ['Ground', 'Poison']
+/* variable pokemonRepository to hold what the IIFE will return 
+and IIFE assigned to this variable */
+let pokemonRepository = (function () {
+    // Pokemon list - an array of Pokémon objects used throughout the web app
+    let pokemonList = [
+        {
+            name: 'Bulbasaur', 
+            height: 0.7, 
+            types: ['Grass', 'Poison']
+        }, 
+        {
+            name: 'Charizard', 
+            height: 1.7, 
+            types: ['Fire', 'Flying']
+        }, 
+        {
+            name: 'Butterfree', 
+            height: 1.1, 
+            types: ['Bug', 'Flying']
+        }, 
+        {
+            name: 'Nidoqueen', 
+            height: 1.3, 
+            types: ['Ground', 'Poison']
+        }
+    ];
+    // Function to return all array items in pokemonList
+    function getAll() {
+        return pokemonList;
     }
-];
+    // Function to add new object to array pokemonList   
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    // Return value of this IIFE, pokemonList
+    return {
+        // key-value pair that represents the add function as its value
+        add: add,
+        // key-value pair that represents the getAll function as its value
+        getAll: getAll
+    };
+})();
+
+// New pokemon
+ pokemonRepository.add({
+    name: 'Pikachu',
+    height: 0.3,
+    types: ['Electric']
+});
+
+
+
 
 //for loop to display the name and height of each pokemon in the array to the DOM
 //add remark to highlight big pokemon
@@ -32,7 +62,7 @@ let pokemonList = [
 } */
 
 //replace above for() loop with forEach() loop
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height < 1.5) {
         document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") " + "</p>")
     } else {
